52f3d1c2de79f4ebd574e9d03ab058938fed1506
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-12-29 20:31:05 +1000

    A -> V [in syntax highlighting]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name = '11l',
     py_modules = ['11l', 'python_to_11l/tokenizer', 'python_to_11l/parse', '_11l_to_cpp/tokenizer', '_11l_to_cpp/parse'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'A Python implementation of transpilers Python → 11l and 11l → C++',
 #    long_description = open('README.md', encoding = 'utf-8').read(),
 #    long_description_content_type = 'text/markdown',
