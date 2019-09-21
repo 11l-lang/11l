ef7ccb0bb132c923053b82249c918a6b90c6f14c
Alexander Tretyak <alextretyak@users.noreply.github.com>
2019-09-21 11:03:12 +1000

    Skip .mypy_cache and .vs directories when packing 11l.tar.xz

--- pack.py
+++ pack.py
@@ -3,7 +3,7 @@ import os, sys
 for envvar in ['ProgramFiles', 'ProgramFiles(x86)', 'ProgramW6432']:
     os.environ["PATH"] += os.pathsep + os.getenv(envvar, '') + r'\7-Zip'
 
-if os.system('7z a -mx9 -xr!__pycache__ -xr!*.exe -xr!*.obj -- 11l.tar 11l 11l.cmd 11l.py LICENSE.txt python_to_11l/*.py python_to_11l/tests/* _11l_to_cpp/*.py _11l_to_cpp/tests/* _11l_to_cpp/11l.hpp _11l_to_cpp/11l_hpp/*') != 0:
+if os.system('7z a -mx9 -xr!__pycache__ -xr!.mypy_cache -xr!.vs -xr!*.exe -xr!*.obj -- 11l.tar 11l 11l.cmd 11l.py LICENSE.txt python_to_11l/*.py python_to_11l/tests/* _11l_to_cpp/*.py _11l_to_cpp/tests/* _11l_to_cpp/11l.hpp _11l_to_cpp/11l_hpp/*') != 0:
     sys.exit('7z failed')
 
 if os.system('7z a -sdel -mx9 -- 11l.tar.xz 11l.tar') != 0:
