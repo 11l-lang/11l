9f4010afd3047094c3aebe85dd38d95dc5d75522
Alexander Tretyak <alextretyak@users.noreply.github.com>
2020-08-20 16:44:51 +1000

    -Error: +Lexical error: +Syntax error:

--- 11l.py
+++ 11l.py
@@ -21,12 +21,12 @@ enopt = not '-d' in sys.argv
 if not (sys.argv[1].endswith('.py') or sys.argv[1].endswith('.11l')):
     sys.exit("source-file should have extension '.py' or '.11l'")
 
-def show_error(fname, fcontents, e):
+def show_error(fname, fcontents, e, syntax_error):
     next_line_pos = fcontents.find("\n", e.pos)
     if next_line_pos == -1:
         next_line_pos = len(fcontents)
     prev_line_pos = fcontents.rfind("\n", 0, e.pos) + 1
-    sys.exit('Error: ' + e.message + "\n in file '" + fname + "', line " + str(fcontents[:e.pos].count("\n") + 1) + "\n"
+    sys.exit(('Syntax' if syntax_error else 'Lexical') + ' error: ' + e.message + "\n in file '" + fname + "', line " + str(fcontents[:e.pos].count("\n") + 1) + "\n"
            + fcontents[prev_line_pos:next_line_pos] + "\n" + re.sub(r'[^\t]', ' ', fcontents[prev_line_pos:e.pos]) + '^'*max(1, e.end - e.pos))
 
 import _11l_to_cpp.tokenizer, _11l_to_cpp.parse
@@ -37,7 +37,7 @@ if sys.argv[1].endswith('.py'):
     try:
         _11l_code = python_to_11l.parse.parse_and_to_str(python_to_11l.tokenizer.tokenize(py_source), py_source, sys.argv[1])
     except (python_to_11l.parse.Error, python_to_11l.tokenizer.Error) as e:
-        show_error(sys.argv[1], py_source, e)
+        show_error(sys.argv[1], py_source, e, type(e) == python_to_11l.parse.Error)
     _11l_fname = os.path.splitext(sys.argv[1])[0] + '.11l'
     open(_11l_fname, 'w', encoding = 'utf-8', newline = "\n").write(_11l_code)
 else:
@@ -49,7 +49,7 @@ try:
     cpp_code += _11l_to_cpp.parse.parse_and_to_str(_11l_to_cpp.tokenizer.tokenize(_11l_code), _11l_code, _11l_fname, append_main = True)
 except (_11l_to_cpp.parse.Error, _11l_to_cpp.tokenizer.Error) as e:
     # open(_11l_fname, 'w', encoding = 'utf-8', newline = "\n").write(_11l_code)
-    show_error(_11l_fname, _11l_code, e)
+    show_error(_11l_fname, _11l_code, e, type(e) == _11l_to_cpp.parse.Error)
 
 cpp_fname = os.path.splitext(sys.argv[1])[0] + '.cpp'
 open(cpp_fname, 'w', encoding = 'utf-8-sig', newline = "\n").write(cpp_code) # utf-8-sig is for MSVC
